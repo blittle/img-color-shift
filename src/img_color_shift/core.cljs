aec6e638e505b40e96c610395786d3005d34fc87
Bret Little <bret.little@gmail.com>
2014-08-15 07:40:34 -0600

    Correctly rendering an image

--- src/img_color_shift/core.cljs
+++ src/img_color_shift/core.cljs
@@ -1,8 +1,32 @@
 (ns img-color-shift.core)
 
-(defn greeting  
-    ([] (greeting "World!"))
-    ([name] (str "Hello, " name)))
+(defn onImageLoad ([] 
+    (this-as this
+        (drawImage this (canvas-context 
+            (.-width this) 
+            (.-height this))))))
 
-(defn ^:export main []  
-    (.write js/document (greeting "Breet")))
+(defn drawImage ([image, context] 
+    (.drawImage context image 0 0)))
+
+(defn loadImage ([url] ( 
+    let [imgr (js/Image.)]
+    (set! (.-onload imgr) onImageLoad)
+    (set! (.-src imgr) url)
+    imgr)))
+
+(defn canvas-context ([width, height] 
+    (.getContext 
+        (create-canvas width height) "2d")))
+
+(defn create-canvas ([width, height] (
+    let [canvas (js/document.createElement "canvas")]
+    (set! (.-width canvas) width)
+    (set! (.-height canvas) height)
+    (.appendChild js/document.body canvas)
+    canvas)))
+
+(defn ^:export buildImage [url]  
+    (loadImage url))
+
+(buildImage "http://apod.nasa.gov/apod/image/1408/Supermoon_20140810.JPG")
